f62a3d927724312e3f136cb7c5e592d45758bdbd
lfs <liufangshuo@qq.com>
2017-04-17 11:28:32 +0800

    去掉主题

--- config/db.php
+++ config/db.php
@@ -2,32 +2,23 @@
 
 //数据库根据主题来配置
 if (YII_ENV_PROD) {
-    $dbs = [
-        'basic' =>
-            [
-                'class' => 'yii\db\Connection',
-                'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-                'username' => 'root',
-                'password' => '',
-                'charset' => 'utf8',
-            ]
+    $db = [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
     ];
-
-    $db = $dbs[THEME_NAME];
-
+    
 } else {
-    $dbs = [
-        'basic' =>[
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=lfscms',
-            'username' => 'root',
-            'password' => '1',
-            'charset' => 'utf8',
-            'tablePrefix' => 'lfs_'
-        ]
+    $db = [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=lfscms',
+        'username' => 'root',
+        'password' => '1',
+        'charset' => 'utf8',
+        'tablePrefix' => 'lfs_'
     ];
-
-    $db = $dbs[THEME_NAME];
 }
 
 return $db;
--- config/mail.php
+++ config/mail.php
@@ -8,25 +8,7 @@
 
 
 if(YII_ENV_DEV){
-    $emails = [
-        'basic' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => EMAIL_SERVICE,
-                'username' => EMAIL_USER,
-                'password' => EMAIL_PWD,
-                'port' => EMAIL_PORT,
-            ],
-        ]
-    ];
-
-    return isset($emails[THEME_NAME]) ? $emails[THEME_NAME] : [];
-}
-
-$emails = [
-    'basic' => [
+    $email= [
         'class' => 'yii\swiftmailer\Mailer',
         'useFileTransport' => false,
         'transport' => [
@@ -35,9 +17,23 @@ $emails = [
             'username' => EMAIL_USER,
             'password' => EMAIL_PWD,
             'port' => EMAIL_PORT,
-        ],
+        ]
+    ];
+
+    return $email;
+}
+
+$email = [
+    'class' => 'yii\swiftmailer\Mailer',
+    'useFileTransport' => false,
+    'transport' => [
+        'class' => 'Swift_SmtpTransport',
+        'host' => EMAIL_SERVICE,
+        'username' => EMAIL_USER,
+        'password' => EMAIL_PWD,
+        'port' => EMAIL_PORT,
     ],
 ];
 
-return isset($emails[THEME_NAME]) ? $emails[THEME_NAME] : [];
+return $email;
 
