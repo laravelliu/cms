9dec7683f6a796cbc892eea1925ca25a2919ed5f
lfs <liufangshuo@qq.com>
2017-07-11 17:33:26 +0800

    数据库更换

--- config/db.php
+++ config/db.php
@@ -4,9 +4,9 @@
 if (YII_ENV_PROD) {
     $db = [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=lfscms',
         'username' => 'root',
-        'password' => '',
+        'password' => 'Lfs@qq.com',
         'charset' => 'utf8',
     ];
     
