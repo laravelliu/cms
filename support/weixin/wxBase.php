883f67b55b217670b063e63df6997c6a31dd6fbc
lfs <liufangshuo@qq.com>
2017-08-08 11:58:40 +0800

    微信

--- support/weixin/wxBase.php
+++ support/weixin/wxBase.php
@@ -13,11 +13,22 @@ use yii\base\Component;
 
 class wxBase extends Component
 {
-    public $AppID = '';
-    public $AppSecret = '';
+    public $AppID = 'wx68b0e4f75b56210d';
+    public $AppSecret = '37bd18487e34a8c4e4156765f6193ab8';
 
-    protected $acccess_token;
+    protected $access_token;
+    protected $access_token_time = null;
 
+    public function __construct(array $config)
+    {
+        parent::__construct($config);
+
+        //获取token
+        if(empty($this->access_token_time) || time() >= $this->access_token_time)
+        {
+            $this->getToken();
+        }
+    }
 
     /**
      * 获取token
@@ -34,6 +45,7 @@ class wxBase extends Component
             $token = Yii::$app->session->get(WX_TOKEN);
         } else  {
             $token = null;
+            $tokenTime = $time;
             $model = new curl\Curl();
 
             //默认直接json_decode
@@ -43,13 +55,17 @@ class wxBase extends Component
 
                 if (isset($resArr['access_token'])) {
                     $token = $resArr['access_token'];
+                    $tokenTime = $resArr['expires_in']+$time;
 
                     //存储在session
                     Yii::$app->session->set(WX_TOKEN, $resArr['access_token']);
-                    Yii::$app->session->set(WX_TOKEN_TIME, $resArr['expires_in']+$time);
+                    Yii::$app->session->set(WX_TOKEN_TIME, $tokenTime);
                 }
             }
         }
+
+        $this->access_token = $token;
+        $this->access_token_time = $tokenTime;
 
         return $token;
     }
@@ -58,10 +74,10 @@ class wxBase extends Component
      * 获取ips
      * @author: liuFangShuo
      */
-    public function getWxIps($token)
+    public function getWxIps()
     {
         $model = new curl\Curl();
-        $resArr = $model->get(WX_URL."/cgi-bin/getcallbackip?access_token=".$token);
+        $resArr = $model->get(WX_URL."/cgi-bin/getcallbackip?access_token=".$this->acccess_token);
 
         $ips = [];
         if (!empty($resArr)) {
