2f6acdd1cd8d9aafb4b13411afcefd96e63bf98b
lfs <liufangshuo@qq.com>
2018-01-04 08:59:38 +0800

    测试回掉地址

--- controllers/LfsController.php
+++ controllers/LfsController.php
@@ -4,10 +4,28 @@ namespace app\controllers;
 use yii;
 class LfsController extends \yii\web\Controller
 {
+    public $key = 'CGSISIJM';
+
     public function actionIndex()
     {
         $data = Yii::$app->request->get();
-        var_dump($data);exit;
+        Yii::info(json_encode($data));
     }
 
+    public function actionCurl()
+    {
+        $arr = [];
+        $code = Yii::$app->request->get('code',null);
+        $arr[] = $code;
+        $arr[] = $this->key;
+        sort($arr);
+        $str = implode(',',$arr);
+        //转码utf-8
+        $encode = mb_detect_encoding($str, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5'));
+        $str_encode = mb_convert_encoding($str, 'UTF-8', $encode);
+
+        $sign = base64_encode(md5($str_encode));
+        var_dump($str);
+        var_dump($str_encode);exit;
+    }
 }
